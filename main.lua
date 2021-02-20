247e13276a11728c0579698aa16de847b77fbae3
necobuba <65777062+necobuba@users.noreply.github.com>
2021-02-20 19:57:01 +0300

    main added

---
+++ main.lua
@@ -0,0 +1,15 @@
+if game.GameId == 257061226 or game.GameId == 115797356 game.GameId == 228573408 then
+if game.GameId == 228573408 then
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/necobuba/adolfhub/main/superhero.lua'),true))()
+end
+
+if game.GameId == 115797356 then
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/necobuba/adolfhub/main/cbro.lua'),true))()
+end
+
+if game.GameId == 257061226 then
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/necobuba/adolfhub/main/site17.lua'),true))()
+end
+else
+print("NOT FOUND")
+end
