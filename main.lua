4efb54093a88aee7667cd67200557c9071a18085
necobuba <65777062+necobuba@users.noreply.github.com>
2021-02-28 13:59:58 +0300

    Added TESİS4

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-if game.GameId == 257061226 or game.GameId == 115797356 or game.GameId == 228573408 or game.GameId == 2265345016 or game.GameId == 1071153038 then
+if game.GameId == 257061226 or game.GameId == 115797356 or game.GameId == 228573408 or game.GameId == 2265345016 or game.GameId == 1071153038 or game.PlaceId == 2384168338 then
 if game.GameId == 228573408 then
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/necobuba/adolfhub/main/superhero.lua'),true))()
 end
@@ -13,6 +13,9 @@ loadstring(game:HttpGet(('https://raw.githubusercontent.com/necobuba/adolfhub/ma
 end
 if game.GameId == 1071153038 then
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/necobuba/adolfhub/main/brawlst.lua'),true))()
+end
+if game.PlaceId == 2384168338 then
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/necobuba/adolfhub/main/tesis4.lua'),true))()
 end
 else
 print("NOT FOUND")
