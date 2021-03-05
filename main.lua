1f83027538bc2d1500aae5f087a78cbbb7f26982
necobuba <65777062+necobuba@users.noreply.github.com>
2021-03-05 21:57:36 +0300

    [SAVAŞ 💂🏼 ] Türk Ordusu Simülasyonu added

--- main.lua
+++ main.lua
@@ -1,21 +1,24 @@
-if game.GameId == 257061226 or game.GameId == 115797356 or game.GameId == 228573408 or game.GameId == 2265345016 or game.GameId == 1071153038 or game.PlaceId == 2384168338 then
+if game.GameId == 257061226 or game.GameId == 115797356 or game.GameId == 228573408 or game.GameId == 2265345016 or game.GameId == 1071153038 or game.PlaceId == 2384168338 or game.GameId == 1167440934 then
 if game.GameId == 228573408 then
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/necobuba/adolfhub/main/superhero.lua'),true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/necobuba/adolfhub/main/superhero.lua'))()
 end
 if game.GameId == 115797356 then
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/necobuba/adolfhub/main/cbro.lua'),true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/necobuba/adolfhub/main/cbro.lua'))()
 end
 if game.GameId == 257061226 then
-print("NOT FOUND") --loadstring(game:HttpGet(('https://raw.githubusercontent.com/necobuba/adolfhub/main/site17.lua'),true))()
+print("NOT FOUND") --loadstring(game:HttpGet('https://raw.githubusercontent.com/necobuba/adolfhub/main/site17.lua'))()
 end
 if game.GameId == 2265345016 then
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/necobuba/adolfhub/main/fortnite.lua'),true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/necobuba/adolfhub/main/fortnite.lua'))()
 end
 if game.GameId == 1071153038 then
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/necobuba/adolfhub/main/brawlst.lua'),true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/necobuba/adolfhub/main/brawlst.lua'))()
 end
 if game.PlaceId == 2384168338 then
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/necobuba/adolfhub/main/tesis4.lua'),true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/necobuba/adolfhub/main/tesis4.lua'))()
+end
+if game.GameId == 1167440934 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/necobuba/adolfhub/main/TA.lua'))()
 end
 else
 print("NOT FOUND")
