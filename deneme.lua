857dc63eaddf596e4f5d9eb2dec8054d3576efc4
necobuba <65777062+necobuba@users.noreply.github.com>
2021-04-01 15:28:37 +0300

    rwr
    erewrew

---
+++ deneme.lua
@@ -0,0 +1,22 @@
+
+local id = "5679889233"
+local hedef = "BakBen_DeliyimHa"
+
+
+
+
+
+--------------------------------------------------------------------------
+local args = {
+    [1] = workspace.Map.SinirDuvarlar["SınırArabaGeçiti (LVL 3)"].Parti.gecis.Value,
+    [2] = "}, { ",
+    [3] = {
+        ["Volume"] = 1,
+        ["SoundId"] = "rbxassetid://"..id,
+        ["Replicate"] = true,
+        ["Position"] = workspace[hedef].Head,
+        ["Effects"] = true,
+    },
+}
+
+game:GetService("ReplicatedStorage").ChatSystemMakeMessage:FireServer(unpack(args))
